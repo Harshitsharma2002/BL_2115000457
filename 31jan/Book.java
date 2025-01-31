707e87497132bce10777d4879f151bf88e10fe83
Harshit-sharma-cs21 <hrsharma8171087251@gmail.com>
2025-01-31 23:34:18 +0530

    file added

---
+++ 31jan/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+    public Book(String title,String author,int price){
+        this.author=author;
+        this.price=price;
+        this.title=title;
+    }
+    public void display(){
+        System.out.println("title "+title);
+        System.out.println("author "+author);
+        System.out.println("price "+price);
+    }
+    public static void main(String[] args) {
+        Book b=new Book("Harray Potter","Harshit",1000000);
+        b.display();
+    }
+}
