e5d74f5a825a044b1f52169ada93f02028d0f176
Harshit-sharma-cs21 <hrsharma8171087251@gmail.com>
2025-02-04 23:29:30 +0530

    file added

---
+++ 4feb/Student.java
@@ -0,0 +1,28 @@
+public class Student{
+    static String universityName="GLA University";
+    private String name;
+    private char grade;
+    final private int rollNumber;
+    static int totalStudents=0;
+    public Student(String name, char grade, int rollNumber){
+    this.name=name;
+    this.grade=grade;
+    this.rollNumber=rollNumber;
+    totalStudents++;
+    }
+    public void displayTotalStudents(){
+    System.out.println("Total Students: "+totalStudents);
+    }
+    public void Display(){
+    System.out.println("University Name: "+universityName);
+    System.out.println("Student name: "+name);
+    System.out.println("Student grade: "+grade);
+    System.out.println("Student roll Number: "+rollNumber);
+    }
+    public static void main(String [] args){
+    Student s=new Student("Ajay",'A',2115000001);
+    System.out.println(s instanceof Student);
+    s.displayTotalStudents();
+    s.Display();
+    }
+    }
